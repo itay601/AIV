4d1481f6305f550ee78f5c153acebdf39080711b
itay601 <itaymerel1212@gmail.com>
2024-12-25 19:17:00 +0200

    DNS packets check functionality

--- BasicSniffer/PacketsSniffer/Core/Database/SamplesSignitures/SignituresDataClasses.cs
+++ BasicSniffer/PacketsSniffer/Core/Database/SamplesSignitures/SignituresDataClasses.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PacketsSniffer.Core.Detection;
 
 namespace PacketsSniffer.Core.Database.SamplesSignitures
 {
@@ -10,6 +11,7 @@ namespace PacketsSniffer.Core.Database.SamplesSignitures
     {
         public string Domain { get; set; }
         public ushort Type { get; set; }
+        public ThreatPacketsAnalyzer.DnsRecordType RecordType { get; internal set; }
     }
     public class Hashes
     {
