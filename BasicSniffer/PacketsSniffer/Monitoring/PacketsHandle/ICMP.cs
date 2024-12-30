9e374803acfbc757615555824416002cc4c37305
itay601 <itaymerel1212@gmail.com>
2024-12-30 21:30:33 +0200

    add ICMP class with defualt funcs

---
+++ BasicSniffer/PacketsSniffer/Monitoring/PacketsHandle/ICMP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketsSniffer.Monitoring.PacketsHandle
+{
+    class ICMP
+    {
+        public void OversizedPingPacket()
+        {
+
+        }
+        public void TunnelingPattern()
+        {
+
+        }
+        //using Pre-Trained AutoEncode
+        public void RequestReplyAnomalies()
+        {
+
+        }
+        public void AbnormalICMPTypeUsage()
+        {
+
+        }
+    }
+}
